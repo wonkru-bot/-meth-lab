c10e7f6a73ab4a3a724407e5f2b61b4e3649bb3f
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2020-08-19 12:19:22 +0530

    Update sample_config.py

--- miakhalifa/sample_config.py
+++ miakhalifa/sample_config.py
@@ -24,7 +24,7 @@ class Config(object):
     NO_LOAD = ['translation', 'rss']
     WEBHOOK = False
     URL = None
-    FILTER_LIM = os.environ.get('FILTER_LIMIT', None)
+    FILTER_LIMIT = os.environ.get('FILTER_LIMIT', None)
 
     # OPTIONAL
     #ID Seperation format [1,2,3,4]
